efb41264c4994930935cba861ea826123332d7f7
Emilio Gozo <emil.gozo@gmail.com>
2021-03-11 12:00:03 +0800

    repactor: remove jQuery dependencies

--- components/head.php
+++ components/head.php
@@ -1,10 +1,27 @@
 <?php 
 
-include_once('./components/helper.php'); 
+function getTitle($curPage, $pages)
+{
+  $npage = count($pages);
+  for ($p = 0; $p < $npage; $p++) {
+    if ($curPage == $pages[$p][0] . '.php') {
+      return $pages[$p][3];
+    }
+  }
+  return "Weather Watch Initiative - Manila Observatory";
+}
 
 $curPage = basename($_SERVER['PHP_SELF']);
 $curStyle = './wwi-styles/' . str_replace(".php", ".css", $curPage);
 $curScript = './wwi-scripts/' . str_replace(".php", ".js", $curPage);
+
+if ($_GET['view']=="draft") {
+  $imgSrc = "draft";
+  $simgSrc = "sdraft";
+} else {
+  $imgSrc = "public";
+  $simgSrc = "static";
+}
 
 ?>
 
--- reports.php
+++ reports.php
@@ -1,5 +1,3 @@
-<?php include_once('./components/helper.php'); ?>
-
 <!doctype html>
 <html lang="en">
 <?php include_once('./components/head.php'); ?>
@@ -7,25 +5,29 @@
 <body class="bg-gray-600 w-4/5 mx-auto">
     <?php include_once('./components/header.php'); ?>
     <div class="bg-gray-300 border-l border-r border-b border-black flex flex-col">
-        <div class="mx-2">
-            <div id="report-img" class="mx-auto p-3"></div>
+        <div class="m-4">
+            <?php
+            $imgs = glob('./resources/reports/img/'. $imgSrc  .'/*.png');
+            foreach($imgs as $img) {
+                print '<img class="mb-2 border border-black" src="'. $img. '" />';
+            }
+            ?>
         </div>
         <div class="mx-4 border border-black">
             <h1 class="col-lg-12 p-3 display-6"><strong>Additional Information</strong></h1>
         </div>
-        <div class="mx-2">
-            <div id="report-static-img" class="mx-auto p-3"></div>
+        <div class="m-4">
+        <?php
+            $imgs = glob('./resources/reports/img/'. $simgSrc  .'/*.png');
+            foreach($imgs as $img) {
+                print '<img class="mb-2 border border-black" src="'. $img. '" />';
+            }
+            ?>
         </div>
     </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
+    <!-- Main Script -->
     <script src="dist/main.js" type="text/javascript"></script>
-    <script src="<?php echo $curScript; ?>" type="text/javascript"></script>
 </body>
 
 </html>
