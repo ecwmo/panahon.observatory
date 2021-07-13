dc4ef33138c328867cf767c087a4954a550ac25f
Emilio Gozo <emil.gozo@gmail.com>
2021-07-13 16:39:23 +0800

    feat: several model page improvements

--- index.php
+++ index.php
@@ -18,19 +18,19 @@
         x-data="modelSelect()">
         <div class="flex flex-col">
             <!-- Forecast Length -->
-            <div class="flex flex-col items-center space-y-4 px-6" x-show="isFcstTimeVisible">
+            <div class="flex flex-col items-center space-y-2 px-6" x-show="isFcstTimeVisible">
                 <h3 class="text-center text-2xl font-semibold mt-4 mb-2">Forecast Length</h3>
-                <div class="flex flex-row text-sm">
+                <div class="flex flex-row text-xs">
                     <template x-for="(ft, i) in fcstTimes" :key="i">
-                        <div class="flex text-gray-200 justify-center p-2"
-                            :class="{'rounded-l-lg pl-3': i === 0, 'rounded-r-lg pr-3': i === 2, 'border-l border-r border-gray-200': (i > 0) && (i < 2), 'bg-blue-600': ft.val === fcstTime, 'cursor-pointer bg-gray-700 hover:bg-gray-500 hover:text-white': ft.val !== fcstTime}"
+                        <div class="flex text-gray-200 justify-center p-1"
+                            :class="{'rounded-l-lg pl-3': i === 0, 'rounded-r-lg pr-3': i === 4, 'border-l border-r border-gray-200': (i > 0) && (i < 4), 'bg-blue-600': ft.val === fcstTime, 'cursor-pointer bg-gray-700 hover:bg-gray-500 hover:text-white': ft.val !== fcstTime}"
                             @click="setFcstTime(ft.val)" x-text="ft.text">
                         </div>
                     </template>
                 </div>
             </div>
             <!-- Fields -->
-            <div class="flex flex-col items-center space-y-4 px-6 min-w-max w-2/5 md:w-full mx-auto">
+            <div class="flex flex-col items-center space-y-2 px-6 min-w-max w-2/5 md:w-full mx-auto">
                 <h3 class="text-center text-2xl font-semibold mt-4 mb-2">Fields</h3>
                 <template x-for="mf in metFields" :key="mf.varName">
                     <div class="w-full flex justify-center text-gray-200 font-bold py-2 px-4 rounded-lg"
@@ -40,15 +40,19 @@
                 </template>
             </div>
         </div>
-        <div class="flex flex-col space-y-2 mx-12">
-            <h2 class="text-center font-semibold text-3xl">
-                <?php print $modelPageHeader; ?>
-            </h2>
+        <div class="flex flex-col space-y-2 mx-12 md:w-full">
+            <h2 class="text-center font-semibold text-3xl" , x-text="headerName"></h2>
             <div class="flex flex-col justify-center px-4">
                 <img class="object-center" :src="`resources/model/img/${getImgName()}`" />
             </div>
-            <div class="text-sm mx-5 font-semibold text-justify break-words md:break-normal model-caption"
+            <div class="text-sm mx-5 font-semibold text-justify self-center break-words md:break-normal model-caption w-11/12"
                 x-html="caption" x-show="caption"></div>
+            <div
+                class="italic text-sm mx-5 font-medium text-justify self-center break-words md:break-normal model-caption w-11/12">
+                <span class="font-bold">DISCLAIMER</span>: This website contains experimental forecasts for research
+                purposes. For official updates and
+                warnings, please refer to PAGASA and other government agencies.
+            </div>
         </div>
     </div>
 
--- src/js/ModelPage.js
+++ src/js/ModelPage.js
@@ -1,42 +1,85 @@
 const metFields = [
-  { varName: "rain", text: "Rainfall", hasFcstTime: true },
+  { varName: "rain", text: "Daily Rainfall", hasFcstTime: true },
   { varName: "t2", text: "Temperature", hasFcstTime: true },
-  { varName: "rh", text: "Relative Humidity", hasFcstTime: true },
-  { varName: "winds", text: "Wind Speed", hasFcstTime: true },
   {
-    varName: "heat_index",
+    varName: "hi",
     text: "Heat Index",
+    hasFcstTime: true,
+    caption: `
+    The figure above shows the spatial map of forecast heat stress index (HI). 
+    The HI calculation is based on 
+      <a 
+        href="https://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml"
+        target="_blank" rel="noopener noreferrer">Rothfusz (1990)</a>.
+      <img src="resources/model/static/heat_index_static_table.png" class="model-caption-img" />`
+  },
+  { varName: "rh", text: "Relative Humidity", hasFcstTime: true },
+  { varName: "winds", text: "Winds", hasFcstTime: true },
+  {
+    varName: "ts_forecast",
+    text: "Hourly Forecasts",
     hasFcstTime: false,
-    caption:
-      'The figure above shows the time series of 72-hour forecast heat stress index (HI) and the corresponding monthly climatological HI from the Manila Observatory weather station. The HI calculation is based on <a href="https://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml" target="_blank" rel="noopener noreferrer">Rothfusz (1990)</a>. HI values at temperatures below 26.67 degrees Celsius and relative humidity values below 40 % are masked. The monthly climatological HI values are based on the hourly temperature and relative humidity data from the Manila Observatory weather station from 2010 to 2019.'
+    headerName: "Hourly Forecasts"
+  },
+  {
+    varName: "wpd",
+    text: "Wind Power Forecast",
+    hasFcstTime: true,
+    caption: `
+    The Wind Power Potential plot uses 100-m wind speed of the model output to approximate the wind 
+    speed at 80-m, the typical height of wind turbines.
+    `
+  },
+  {
+    varName: "ppv",
+    text: "Solar Power Forecast",
+    hasFcstTime: true,
+    caption: `
+    The equations used for calculating Photovoltaic Power Potential were based on 
+      <a 
+        href="https://pubs.rsc.org/lv/content/articlelanding/2011/ee/c1ee01495a/unauth#!divAbstract"
+        target="_blank" rel="noopener noreferrer">Crook et al. (2011)</a>.`
   }
 ];
 
 const fcstTimes = [
   { val: 24, text: "24hr" },
   { val: 48, text: "48hr" },
-  { val: 72, text: "72hr" }
+  { val: 72, text: "72hr" },
+  { val: 96, text: "96hr" },
+  { val: 120, text: "120hr" }
 ];
 
 function modelSelect() {
   return {
+    defaultHeaderName: "Model Forecast Maps",
+    headerName: "Model Forecast Maps",
     varName: "rain",
     fcstTime: 24,
     isFcstTimeVisible: true,
     caption: null,
     setVarName(val) {
       this.varName = val;
-      const { caption, hasFcstTime } = (this.isFcstTimeVisible = metFields.find(
+      const {
+        caption,
+        hasFcstTime,
+        headerName
+      } = (this.isFcstTimeVisible = metFields.find(
         ({ varName }) => varName == this.varName
       ));
       this.isFcstTimeVisible = hasFcstTime;
       this.caption = caption;
+      this.headerName = headerName || this.defaultHeaderName;
     },
     setFcstTime(val) {
       this.fcstTime = val;
     },
     getImgName() {
-      if (this.varName === "heat_index") return `${this.varName}.png`;
+      if (!this.isFcstTimeVisible) return `${this.varName}.png`;
+      else if (this.varName === "rain") {
+        let idx = fcstTimes.findIndex(({ val }) => val === this.fcstTime) + 1;
+        return `wrf-24hr_${this.varName}_day${idx}.png`;
+      }
       return `wrf-${this.fcstTime}hr_${this.varName}.png`;
     }
   };
