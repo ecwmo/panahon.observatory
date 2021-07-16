66d07871829a50880f810e0436a4a63b0a83321d
Emilio Gozo <emil.gozo@gmail.com>
2021-07-16 18:49:17 +0800

    fix: code cleanup

--- src/js/QuickViewPage.js
+++ src/js/QuickViewPage.js
@@ -1,6 +1,6 @@
 // import { stationList } from "../data/station";
-import * as stationMapPH from "../../resources/data/station_map_ph.json";
-import * as stationMapMM from "../../resources/data/station_map_mm.json";
+import stationMapPH from "../../resources/data/station_map_ph.json";
+import stationMapMM from "../../resources/data/station_map_mm.json";
 
 const PH_MAP = "./resources/static/map/ph400.png";
 const MM_MAP = "./resources/static/map/mm400.png";
@@ -9,22 +9,22 @@ function stationSelect() {
   return {
     mapSrc: PH_MAP,
     mapAlt: "Topographical Map of the Philippines",
-    stationPts: stationMapPH.data,
+    stationPts: stationMapPH,
     activeVarPanel: "rain",
     changeMap(name) {
       if (name === "mm") {
         this.mapSrc = MM_MAP;
         this.mapAlt = "Topographical Map of Metro Manila and Neighborhood";
-        this.stationPts = stationMapMM.data;
+        this.stationPts = stationMapMM;
       } else {
         this.mapSrc = PH_MAP;
         this.mapAlt = "Topographical Map of the Philippines";
-        this.stationPts = stationMapPH.data;
+        this.stationPts = stationMapPH;
       }
     },
     setActiveVarPanel(varName) {
       this.activeVarPanel = varName;
-    }
+    },
   };
 }
 
