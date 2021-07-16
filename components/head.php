72fe02ae1cd7b7a9e923533c4c5c315e81b87cfe
Emilio Gozo <emil.gozo@gmail.com>
2021-07-16 20:25:46 +0800

    fix: fontawesome icon not loading

--- components/head.php
+++ components/head.php
@@ -36,7 +36,9 @@
     <link rel="shortcut icon" href="/resources/static/logo.png">
 
     <?php if($curPage == "new-report.php") { ?>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/solid.min.css" />
+
     <?php } ?>
     <link href="/dist/styles.css" rel="stylesheet" type="text/css" />
 </head>
