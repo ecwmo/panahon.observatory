6ba5b2e351c0c1339dd1e62fafa19ec801f7aaa8
Emilio Gozo <emil.gozo@gmail.com>
2021-09-04 21:12:48 +0800

    feat: forecast imgs api endpoint

---
+++ api/forecast-img.php
@@ -0,0 +1,20 @@
+<?php
+    include_once(__DIR__.'/../config.php');
+
+    $ROOT_IMG_DIR = '../resources/model/web_img';
+
+    $imgSrc = $ROOT_IMG_DIR;
+    $cmapImgSrc = $ROOT_IMG_DIR.'/cmap';
+
+    $imgs = glob($imgSrc  .'/*.png', GLOB_BRACE);
+    $cmapImgs = glob($cmapImgSrc  .'/*.png', GLOB_BRACE);
+
+    $data = [
+        "imgs" => $imgs,
+        "cmaps" => $cmapImgs
+    ];
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    echo str_replace("..", "", json_encode($data));
