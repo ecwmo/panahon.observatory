ce10c1e469760cc2be1a3e5701dc45430d9da521
Emilio Gozo <emil.gozo@gmail.com>
2021-09-28 10:54:17 +0800

    style: white header text

--- components/header.php
+++ components/header.php
@@ -1,7 +1,7 @@
-<header class="bg-blue-900 pt-1 border border-black md:flex-col flex flex-row justify-between shadow-lg">
+<header class="bg-blue-900 pt-1 border border-black md:flex-col flex flex-row justify-between shadow-lg text-gray-200">
     <div class="flex text-xs m-1 mb-2">
         <img alt="Manila Observatory Logo" class="h-12 ml-1" src="/resources/static/logo.png" />
-        <p class="md:hidden font-bold text-2xl ml-3 my-auto text-gray-200">Manila Observatory</p>
+        <p class="md:hidden font-bold text-2xl ml-3 my-auto ">Manila Observatory</p>
         <div class="hidden md:inline px-2">
             <p class="font-bold">Manila Observatory</p>
             <p>Ateneo de Manila University Campus</p>
@@ -10,7 +10,7 @@
         <div class="hidden md:inline border-l border-black px-2">
             <p>Tel: (632) 8426-5921 / 8426-0837 / 8426-6495</p>
             <p>Fax: (632) 8426-0847 / 8426-6141</p>
-            <p>Email: <a href="mailto:manila@observatory.ph" title="Manila Observatory email address" class="text-gray-200">manila@observatory.ph</a></p>
+            <p>Email: <a href="mailto:manila@observatory.ph" title="Manila Observatory email address" class="hover:text-blue-400">manila@observatory.ph</a></p>
         </div>
     </div>
     <?php include_once(__DIR__.'/../components/nav.php'); ?>
