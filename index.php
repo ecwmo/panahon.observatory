c414e7423ad0117cecb6fba09cf1052b313eebe3
Emilio Gozo <emil.gozo@gmail.com>
2022-03-21 12:26:15 +0800

    style: lg and med viewport

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <?php include_once('./components/head.php'); ?>
 
-<body class="bg-gray-600 w-full md:w-11/12 mx-auto">
+<body class="bg-gray-600 w-full md:w-11/12 lg:w-5/6 mx-auto">
   <div id="app" >
   </div>
 </body>
