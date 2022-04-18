40085aa31d677dc784300e23cd94d49c00c16760
Emilio Gozo <emil.gozo@gmail.com>
2022-04-18 10:44:40 +0800

    chore: modify caption

--- src/components/caption/ModelRainx.vue
+++ src/components/caption/ModelRainx.vue
@@ -3,11 +3,13 @@
   based on data from TRMM Multi-Satellite Precipitation Analysis TMPA (3B42RT;
   <a href="https://doi.org/10.5067/TRMM/TMPA/DAY-E/7" target="_blank" rel="noopener noreferrer">
     10.5067/TRMM/TMPA/DAY-E/7 </a
-  >). However, caution is advised in interpreting the maps. Since TRMM underestimates rainfall for the Philippines (<a
+  >). However, caution is advised in interpreting the maps. Since TRMM is observed to underestimate rainfall in the
+  Philippines (<a
     href="https://journals.ametsoc.org/view/journals/hydr/21/7/jhmD190276.xml"
     target="_blank"
     rel="noopener noreferrer"
   >
     Peralta et al., 2020 </a
-  >) a lower extreme threshold value can result in more areas forecast to receive extreme rainfall.
+  >) a lower baseline for monthly rainfall derived from TRMM may result in more areas forecast to receive extreme
+  rainfall.
 </template>
