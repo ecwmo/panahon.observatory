3ecc549ab3d98a09fe6174d5f32605c6ae47c09a
Emilio Gozo <emil.gozo@gmail.com>
2022-05-10 17:56:43 +0800

    fix: map size

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-300 border border-t-0 border-black flex flex-col items-center">
+  <div class="flex flex-col w-full h-screen bg-gray-300 border border-t-0 border-black items-center">
     <div class="w-full flex flex-col md:flex-row bg-gray-400 border-b border-black text-xs p-3 gap-2 content-center">
       <!-- Map Switcher -->
       <div class="flex gap-1.5">
@@ -18,11 +18,11 @@
         </option>
       </select>
     </div>
-    <div class="w-full flex flex-col-reverse md:flex-row md:items-center justify-center gap-4 p-6">
-      <div v-if="loadMap" ref="mapContainer" class="flex relative" :style="mapStyle">
+    <div class="w-full flex-grow flex flex-col md:flex-row md:items-center justify-center md:gap-4 md:p-4">
+      <div class="relative w-full md:w-1/2 h-full">
         <MapBox
-          v-if="stationLayer"
-          class="flex md:flex md:flex-col m-auto md:mx-0 md:w-full md:h-full"
+          v-show="stationLayer"
+          class="w-full h-full"
           :accessToken="mapAccessToken"
           :data="stationLayer"
           :activeVariable="activeVariable"
@@ -32,7 +32,7 @@
         />
         <loading :active="!mapIsLoaded" :is-full-page="false" />
       </div>
-      <div class="flex text-sm text-center items-center justify-center">
+      <div class="hidden md:flex text-sm text-center items-center justify-center">
         <div class="flex flex-col items-center gap-2 md:gap-4">
           <div class="flex flex-col md:items-start w-full">
             <div class="text-lg font-semibold">{{ activeStation.name }}</div>
@@ -49,7 +49,6 @@
   import { defineAsyncComponent, ref, computed, onMounted } from 'vue'
   import { format } from 'date-fns'
 
-  import { useScreen } from 'vue-screen'
   import { useStationData } from '@/composables/useStationData'
 
   import Loading from 'vue-loading-overlay'
@@ -59,12 +58,10 @@
   const InfoPanel = defineAsyncComponent({ loader: () => import('@/components/InfoPanel.vue') })
 
   const mapAccessToken = <string>import.meta.env.VITE_MAPBOX_TOKEN
-  const screen = useScreen()
   const timestamp = ref(new Date())
   const { stationLayer, fetchData } = useStationData()
   const defaultStationId = '1'
   const mapIsLoaded = ref(false)
-  const mapContainer = ref()
   const mapScope = ref('mm')
   const activeStationId = ref(defaultStationId)
   const activeVariable = ref('temp')
@@ -79,17 +76,6 @@
   )
 
   const formatDate = (strFormat = 'MMMM d, yyyy h:00 bbb') => format(timestamp.value, strFormat)
-
-  const loadMap = computed(() => screen.width > 768)
-
-  const mapStyle = computed(() => {
-    const aspectRatio = 1.2
-    const maxHeight = 540
-    let mapHeight = screen.height
-    mapHeight = mapHeight > maxHeight ? maxHeight : mapHeight
-    const mapWidth = mapHeight / aspectRatio
-    return `height:${mapHeight}px;width:${mapWidth}px;`
-  })
 
   onMounted(async () => {
     await fetchData()
