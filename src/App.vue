edc8825a34673912869630c47b736b33a1d4892f
Emilio Gozo <emil.gozo@gmail.com>
2022-05-11 11:46:41 +0800

    style: set max height to screen height

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <Header></Header>
-  <router-view> </router-view>
+  <div class="flex flex-col max-h-screen">
+    <Header></Header>
+    <router-view> </router-view>
+  </div>
 </template>
 
 <script setup lang="ts">
