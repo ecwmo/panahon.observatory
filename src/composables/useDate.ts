a107ad2f8f0914c28c3cc6e693aa7da70ff6189e
Emilio Gozo <emil.gozo@gmail.com>
2022-06-27 12:22:22 +0800

    fix: check if date is valid

--- src/composables/useDate.ts
+++ src/composables/useDate.ts
@@ -1,7 +1,8 @@
-import { format } from 'date-fns'
+import { format, isValid } from 'date-fns'
 
 export default (dateTime = new Date()) => {
-  const formatDate = (strFormat: string, d = dateTime) => format(d, strFormat)
+  const formatDate = (strFormat: string, d = dateTime) =>
+    isValid(d) ? format(d, strFormat) : format(new Date(), strFormat)
 
   return { formatDate }
 }
--- src/store/station.ts
+++ src/store/station.ts
@@ -4,7 +4,6 @@ import { Station } from '@/types/station'
 
 export const useStationStore = defineStore('station', () => {
   const station = ref(<Station>{})
-  const { formatDate: _formatDate } = useDate()
 
   const update = (newData?: Station) => {
     if (newData) station.value = newData
@@ -12,8 +11,8 @@ export const useStationStore = defineStore('station', () => {
 
   const stationName = computed(() => station.value.name)
   const timestamp = computed(() => new Date(station.value?.obs?.timestamp ?? Date.now()))
-
-  const dateString = (format = 'MMMM d, yyyy h:00 bbb') => _formatDate(format, timestamp.value)
+  const { formatDate } = useDate(timestamp.value)
+  const dateString = (format = 'MMMM d, yyyy h:00 bbb') => formatDate(format)
 
   return { station, update, stationName, timestamp, dateString }
 })
