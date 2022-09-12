600c2f1933ee74bd561782418a68d287b519335c
Emilio Gozo <emil.gozo@gmail.com>
2022-09-12 16:53:25 +0800

    style: add z-index

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col w-full select-none" :class="[{ 'h-screen': $route.name === 'index' }]">
-    <NavBar />
-    <div class="bg-skin-body-fill text-skin-base h-full">
+    <NavBar class="z-10" />
+    <div class="bg-skin-body-fill text-skin-base h-full z-0">
       <router-view />
     </div>
   </div>
