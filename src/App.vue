1b479a2ef8f00771a08960bc6cf669febefd4c85
Emilio Gozo <emil.gozo@gmail.com>
2022-09-20 08:53:03 +0800

    style: set container max-width to 1536px

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="flex flex-col w-full select-none h-screen">
-    <NavBar class="z-10" />
-    <div class="bg-skin-body-fill text-skin-base flex-1 overflow-y-auto z-0">
-      <router-view />
+  <div class="fixed inset-0 bg-skin-body-fill/30" aria-hidden="true" />
+  <div class="flex justify-center items-center">
+    <div class="max-w-screen-2xl flex flex-col w-full select-none h-screen">
+      <NavBar class="z-10" />
+      <div class="bg-skin-body-fill text-skin-base flex-1 overflow-y-auto z-0">
+        <router-view />
+      </div>
+      <Footer class="z-10" />
     </div>
-    <Footer class="z-10" />
   </div>
 </template>
