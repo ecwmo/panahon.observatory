db6e3f2d78ea8bf1b1aa187cf0e573294ba79b6c
Emilio Gozo <emil.gozo@gmail.com>
2022-10-19 20:23:01 +0800

    feat: accept $imgFreq

--- api/forecast.php
+++ api/forecast.php
@@ -5,14 +5,14 @@ include_once(__DIR__ . '/helper.php');
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-$mod_img_dir = '../resources/model';
+$modImgDir = '../resources/model';
 
 $tsPHT = get_latest_date();
 $tsUTC = $tsPHT->copy()->tz('UTC');
 
 if (isset($_GET['img'])) {
     if ($_GET['img'] == "2") {
-        $imgDir = $mod_img_dir . '/web_img';
+        $imgDir = $modImgDir . '/web_img';
         $globPattern = "/*" . $tsPHT->format('Y-m-d_H') . "*.png";
         $imgs = glob($imgDir . $globPattern, GLOB_BRACE);
 
@@ -26,18 +26,19 @@ if (isset($_GET['img'])) {
 
         echo str_replace("..", "", json_encode($data));
     } else {
-        $imgDir = $mod_img_dir . "/img/24hrly/" . $tsUTC->format('Ymd/H');
+        $imgFreq = $_GET['img'] != "" ? $_GET['img'] : "24hrly";
+        $imgDir = $modImgDir . "/img/" . $imgFreq . "/" . $tsUTC->format('Ymd/H');
         $globPattern = "/*.png";
         $imgs = glob($imgDir . $globPattern, GLOB_BRACE);
 
-        $imgDir = $mod_img_dir . "/img";
+        $imgDir = $modImgDir . "/img";
         $globPattern = "/wrf-ts_" . $tsPHT->format('Y-m-d_H') . "PHT.png";
         $imgs2 = glob($imgDir . $globPattern, GLOB_BRACE);
 
         echo str_replace("..", "", json_encode(array_merge($imgs, $imgs2)));
     }
 } else {
-    $file = $mod_img_dir . "/forecast_" . $tsPHT->format('Y-m-d_H') . "PHT.json";
+    $file = $modImgDir . "/forecast_" . $tsPHT->format('Y-m-d_H') . "PHT.json";
     $data = file_get_contents($file);
     $obj = json_decode($data, true);
 
