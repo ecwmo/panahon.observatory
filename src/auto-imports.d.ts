b07fa665b8230f0e83449df9db332702897cff08
Emilio Gozo <emil.gozo@gmail.com>
2023-01-29 23:26:23 +0800

    chore: update several dependencies

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -30,6 +30,8 @@ declare global {
   const nextTick: typeof import('vue')['nextTick']
   const onActivated: typeof import('vue')['onActivated']
   const onBeforeMount: typeof import('vue')['onBeforeMount']
+  const onBeforeRouteLeave: typeof import('vue-router')['onBeforeRouteLeave']
+  const onBeforeRouteUpdate: typeof import('vue-router')['onBeforeRouteUpdate']
   const onBeforeUnmount: typeof import('vue')['onBeforeUnmount']
   const onBeforeUpdate: typeof import('vue')['onBeforeUpdate']
   const onDeactivated: typeof import('vue')['onDeactivated']
@@ -46,6 +48,7 @@ declare global {
   const readonly: typeof import('vue')['readonly']
   const ref: typeof import('vue')['ref']
   const resolveComponent: typeof import('vue')['resolveComponent']
+  const resolveDirective: typeof import('vue')['resolveDirective']
   const shallowReactive: typeof import('vue')['shallowReactive']
   const shallowReadonly: typeof import('vue')['shallowReadonly']
   const shallowRef: typeof import('vue')['shallowRef']
@@ -64,6 +67,7 @@ declare global {
   const useEWBStore: typeof import('./store/ewb')['useEWBStore']
   const useForecastStore: typeof import('./store/forecast')['useForecastStore']
   const useIntersectionObserver: typeof import('@vueuse/core')['useIntersectionObserver']
+  const useLink: typeof import('vue-router')['useLink']
   const useLocation: typeof import('./composables/useLocation')['default']
   const useQuery: typeof import('vue-query')['useQuery']
   const useRoute: typeof import('vue-router')['useRoute']
@@ -82,7 +86,7 @@ declare global {
 }
 // for vue template auto import
 import { UnwrapRef } from 'vue'
-declare module '@vue/runtime-core' {
+declare module 'vue' {
   interface ComponentCustomProperties {
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
     readonly afterAll: UnwrapRef<typeof import('vitest')['afterAll']>
@@ -113,6 +117,8 @@ declare module '@vue/runtime-core' {
     readonly nextTick: UnwrapRef<typeof import('vue')['nextTick']>
     readonly onActivated: UnwrapRef<typeof import('vue')['onActivated']>
     readonly onBeforeMount: UnwrapRef<typeof import('vue')['onBeforeMount']>
+    readonly onBeforeRouteLeave: UnwrapRef<typeof import('vue-router')['onBeforeRouteLeave']>
+    readonly onBeforeRouteUpdate: UnwrapRef<typeof import('vue-router')['onBeforeRouteUpdate']>
     readonly onBeforeUnmount: UnwrapRef<typeof import('vue')['onBeforeUnmount']>
     readonly onBeforeUpdate: UnwrapRef<typeof import('vue')['onBeforeUpdate']>
     readonly onDeactivated: UnwrapRef<typeof import('vue')['onDeactivated']>
@@ -129,6 +135,7 @@ declare module '@vue/runtime-core' {
     readonly readonly: UnwrapRef<typeof import('vue')['readonly']>
     readonly ref: UnwrapRef<typeof import('vue')['ref']>
     readonly resolveComponent: UnwrapRef<typeof import('vue')['resolveComponent']>
+    readonly resolveDirective: UnwrapRef<typeof import('vue')['resolveDirective']>
     readonly shallowReactive: UnwrapRef<typeof import('vue')['shallowReactive']>
     readonly shallowReadonly: UnwrapRef<typeof import('vue')['shallowReadonly']>
     readonly shallowRef: UnwrapRef<typeof import('vue')['shallowRef']>
@@ -147,6 +154,7 @@ declare module '@vue/runtime-core' {
     readonly useEWBStore: UnwrapRef<typeof import('./store/ewb')['useEWBStore']>
     readonly useForecastStore: UnwrapRef<typeof import('./store/forecast')['useForecastStore']>
     readonly useIntersectionObserver: UnwrapRef<typeof import('@vueuse/core')['useIntersectionObserver']>
+    readonly useLink: UnwrapRef<typeof import('vue-router')['useLink']>
     readonly useLocation: UnwrapRef<typeof import('./composables/useLocation')['default']>
     readonly useQuery: UnwrapRef<typeof import('vue-query')['useQuery']>
     readonly useRoute: UnwrapRef<typeof import('vue-router')['useRoute']>
