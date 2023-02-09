d7b74f0c0317b9851fcae99005e7412c9c8c98d4
Emilio Gozo <emil.gozo@gmail.com>
2023-02-09 18:07:55 +0800

    style: improve positioning

--- src/components/ImageModal.vue
+++ src/components/ImageModal.vue
@@ -10,30 +10,32 @@
       @keyup.left="$emit('left')"
     >
       <div class="flex items-center justify-center h-screen">
-        <DialogPanel class="flex flex-col justify-center items-center h-full">
-          <div class="hidden md:block cursor-pointer group" @click.prevent="$emit('up')">
+        <DialogPanel
+          class="flex flex-col relative justify-center items-center h-max md:p-10 drop-shadow-lg backdrop-blur-sm backdrop-opacity-75 rounded-lg"
+        >
+          <div class="hidden md:block md:absolute md:top-0 cursor-pointer group" @click.prevent="$emit('up')">
             <i-fa6-solid-chevron-up
               class="scale-75 text-white opacity-75 group-hover:opacity-100 text-2xl md:text-4xl"
             />
           </div>
 
-          <div class="flex justify-center items-center">
-            <div class="hidden md:block cursor-pointer group" @click.prevent="$emit('left')">
-              <i-fa6-solid-chevron-left
-                class="scale-75 text-white opacity-75 group-hover:opacity-100 text-2xl md:text-4xl"
-              />
-            </div>
-            <div ref="imgEl" class="max-w-xl p-1">
-              <slot />
-            </div>
-            <div class="hidden md:block cursor-pointer group" @click.prevent="$emit('right')">
-              <i-fa6-solid-chevron-right
-                class="scale-75 text-white opacity-75 group-hover:opacity-100 text-2xl md:text-4xl"
-              />
-            </div>
+          <div class="hidden md:block md:absolute md:left-0 cursor-pointer group" @click.prevent="$emit('left')">
+            <i-fa6-solid-chevron-left
+              class="scale-75 text-white opacity-75 group-hover:opacity-100 text-2xl md:text-4xl"
+            />
           </div>
 
-          <div class="hidden md:block cursor-pointer group" @click.prevent="$emit('down')">
+          <div ref="imgEl" class="max-w-xl p-1">
+            <slot />
+          </div>
+
+          <div class="hidden md:block md:absolute md:right-0 cursor-pointer group" @click.prevent="$emit('right')">
+            <i-fa6-solid-chevron-right
+              class="scale-75 text-white opacity-75 group-hover:opacity-100 text-2xl md:text-4xl"
+            />
+          </div>
+
+          <div class="hidden md:block md:absolute md:bottom-0 cursor-pointer group" @click.prevent="$emit('down')">
             <i-fa6-solid-chevron-down
               class="scale-75 text-white opacity-75 group-hover:opacity-100 text-2xl md:text-4xl"
             />
