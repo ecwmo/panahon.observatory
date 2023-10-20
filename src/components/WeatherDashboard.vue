72fde3a5624187fb608aba8971c3538b57ba9691
Emilio Gozo <emiliogozo@proton.me>
2023-10-20 18:41:11 +0800

    fix: decrease precision of values displayed

--- src/components/WeatherDashboard.vue
+++ src/components/WeatherDashboard.vue
@@ -5,13 +5,13 @@
         <div class="bg-gray-700 text-gray-100 p-2">{{ isSuccess ? station?.name : 'No data to display' }}</div>
         <div v-if="isSuccess">
           <div class="flex w-full items-center p-2">
-            <div class="w-1/2 text-5xl font-bold text-center">{{ station?.obs.temp }}&deg;C</div>
+            <div class="w-1/2 text-5xl font-bold text-center">{{ tempStr }}&deg;C</div>
             <div class="w-1/2 flex flex-col text-center border-l">
               <div class="text-xl">
-                <span class="font-bold">HIGH: {{ station?.obs.tx }}&deg;C</span> at {{ txTime }}
+                <span class="font-bold">HIGH: {{ txStr }}&deg;C</span> at {{ txTime }}
               </div>
               <div class="text-xl">
-                <span class="font-bold">LOW: {{ station?.obs.tn }}&deg;C</span> at {{ tnTime }}
+                <span class="font-bold">LOW: {{ tnStr }}&deg;C</span> at {{ tnTime }}
               </div>
             </div>
           </div>
@@ -20,15 +20,15 @@
               <div class="i-mdi-weather-windy text-2xl mr-2"></div>
               <div class="w-2/9 text-lg font-semibold">Wind:</div>
               <div class="w-2/9">
-                <span class="text-lg font-semibold">{{ station?.obs.wspd }}</span> m/s
+                <span class="text-lg font-semibold">{{ wspdStr }}</span> m/s
               </div>
-              <div class="">Gust {{ station?.obs.gust }} m/s</div>
+              <div class="">Gust {{ gustStr }} m/s</div>
             </div>
             <div class="flex items-center py-4 px-2 border-t">
               <div class="i-mdi-water-outline text-2xl mr-2"></div>
               <div class="w-2/9 text-lg font-semibold">Humidity:</div>
               <div class="w-2/9">
-                <span class="text-lg font-semibold">{{ station?.obs.rh }}</span> %
+                <span class="text-lg font-semibold">{{ rhStr }}</span> %
               </div>
               <div v-show="heatIndex" class="">Feels like {{ heatIndex }} &deg;C</div>
             </div>
@@ -36,15 +36,15 @@
               <div class="i-mdi-weather-pouring text-2xl mr-2"></div>
               <div class="w-2/9 text-lg font-semibold">Rain:</div>
               <div class="w-2/9">
-                <span class="text-lg font-semibold">{{ station?.obs.rain }}</span> mm
+                <span class="text-lg font-semibold">{{ rainStr }}</span> mm
               </div>
-              <div class="">24 hour total {{ station?.obs.rainAccum }} mm</div>
+              <div v-show="rainAccStr" class="">24 hour total {{ rainAccStr }} mm</div>
             </div>
-            <div v-show="station?.obs.mslp" class="flex items-center py-4 px-2 border-t">
+            <div v-show="presStr" class="flex items-center py-4 px-2 border-t">
               <div class="i-wi-barometer text-2xl mr-2"></div>
               <div class="w-2/9 text-lg font-semibold">Pressure:</div>
               <div class="w-2/9">
-                <span class="text-lg font-semibold">{{ station?.obs.mslp }}</span> mb
+                <span class="text-lg font-semibold">{{ presStr }}</span> mb
               </div>
             </div>
           </div>
@@ -85,6 +85,16 @@
 
   const dateStr = computed(() => format(parseISO(station.value?.obs?.timestamp ?? ''), 'h:mm a EEEE, MMM d yyyy'))
 
+  const tempStr = computed(() => station.value?.obs.temp?.toFixed(1))
+  const wspdStr = computed(() => station.value?.obs.wspd?.toFixed(1))
+  const gustStr = computed(() => station.value?.obs.gust?.toFixed(1))
+  const rhStr = computed(() => station.value?.obs.rh?.toFixed(0))
+  const rainStr = computed(() => station.value?.obs.rain?.toFixed(0))
+  const rainAccStr = computed(() => station.value?.obs.rainAccum?.toFixed(0))
+  const presStr = computed(() => station.value?.obs.mslp?.toFixed(0))
+
+  const tnStr = computed(() => station.value?.obs.tn?.toFixed(1))
+  const txStr = computed(() => station.value?.obs.tx?.toFixed(1))
   const txTime = computed(() => format(parseISO(station.value?.obs?.txTimestamp ?? ''), 'h:mm a'))
   const tnTime = computed(() => format(parseISO(station.value?.obs?.tnTimestamp ?? ''), 'h:mm a'))
 
