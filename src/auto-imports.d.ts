aea7aba3bc4c658c2d3ee4c62d86d4936c749831
Emilio Gozo <emiliogozo@proton.me>
2024-01-30 14:29:17 +0800

    chore: update deps

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -74,10 +74,12 @@ declare global {
 declare global {
   // @ts-ignore
   export type { Component, ComponentPublicInstance, ComputedRef, ExtractDefaultPropTypes, ExtractPropTypes, ExtractPublicPropTypes, InjectionKey, PropType, Ref, VNode, WritableComputedRef } from 'vue'
+  import('vue')
 }
 // for vue template auto import
 import { UnwrapRef } from 'vue'
 declare module 'vue' {
+  interface GlobalComponents {}
   interface ComponentCustomProperties {
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
     readonly axios: UnwrapRef<typeof import('axios')['default']>
@@ -146,6 +148,7 @@ declare module 'vue' {
   }
 }
 declare module '@vue/runtime-core' {
+  interface GlobalComponents {}
   interface ComponentCustomProperties {
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
     readonly axios: UnwrapRef<typeof import('axios')['default']>
