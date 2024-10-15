b126de233032623b24fa6d38fd2fb9f27875eacb
Emilio Gozo <15209351+atsuyaourt@users.noreply.github.com>
2024-10-15 15:46:14 +0800

    fix: improve type inference

--- src/schemas/common.ts
+++ src/schemas/common.ts
@@ -1,6 +1,6 @@
 import camelcaseKeys from 'camelcase-keys'
 import { z } from 'zod'
 
-export const camelize = <T extends readonly unknown[] | Record<string, unknown>>(val: T) => camelcaseKeys(val)
+export const camelize = <T extends Record<string, unknown>>(val: T) => camelcaseKeys(val)
 
 export const imgSrc = z.string().regex(/\.(jpe?g|png)$/i)
