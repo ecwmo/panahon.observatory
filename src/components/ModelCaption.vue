145fa15adf351943eb564b6a846ac1427dbe48a1
Emilio Gozo <15209351+atsuyaourt@users.noreply.github.com>
2024-10-29 16:31:34 +0800

    fix: ari caption

--- src/components/ModelCaption.vue
+++ src/components/ModelCaption.vue
@@ -21,21 +21,21 @@
       >.
     </span>
     <span v-else-if="id === 'rainx'">
-      The figure above highlights areas with forecast 24-hour rainfall possibly exceeding the typical monthly total
-      rainfall based on data from TRMM Multi-Satellite Precipitation Analysis TMPA (3B42RT;
-      <a href="https://doi.org/10.5067/TRMM/TMPA/DAY-E/7" target="_blank" rel="noopener noreferrer">
-        10.5067/TRMM/TMPA/DAY-E/7 </a
-      >). However, caution is advised in interpreting the maps. Since TRMM is observed to underestimate rainfall in the
-      Philippines (<a
-        href="https://journals.ametsoc.org/view/journals/hydr/21/7/jhmD190276.xml"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Peralta et al., 2020 </a
-      >) a lower baseline for monthly rainfall derived from TRMM may result in more areas forecast to receive extreme
-      rainfall.
-    </span>
-    <span v-else-if="id === 'ari'">
+    <!--   The figure above highlights areas with forecast 24-hour rainfall possibly exceeding the typical monthly total -->
+    <!--   rainfall based on data from TRMM Multi-Satellite Precipitation Analysis TMPA (3B42RT; -->
+    <!--   <a href="https://doi.org/10.5067/TRMM/TMPA/DAY-E/7" target="_blank" rel="noopener noreferrer"> -->
+    <!--     10.5067/TRMM/TMPA/DAY-E/7 </a -->
+    <!--   >). However, caution is advised in interpreting the maps. Since TRMM is observed to underestimate rainfall in the -->
+    <!--   Philippines (<a -->
+    <!--     href="https://journals.ametsoc.org/view/journals/hydr/21/7/jhmD190276.xml" -->
+    <!--     target="_blank" -->
+    <!--     rel="noopener noreferrer" -->
+    <!--   > -->
+    <!--     Peralta et al., 2020 </a -->
+    <!--   >) a lower baseline for monthly rainfall derived from TRMM may result in more areas forecast to receive extreme -->
+    <!--   rainfall. -->
+    <!-- </span> -->
+    <!-- <span v-else-if="id === 'ari'"> -->
       The figure above shows daily rainfall amounts exceeding a 5-year average recurrence interval (ARI) threshold.
       <br />
       <br />
